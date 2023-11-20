0dd9e3f8a2cb83c47c9a5322e01a46be069c2d05
stilt0n <mattias.leino@arts.cornish.edu>
2023-11-20 01:43:26 -0500

    chore: prepare for initial deployment

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,8 @@
+/*
+	This tells SvelteKit to crawl the links on the site and prerender the files at build time.
+*/
+export const prerender = true;
+
 export const load = async ({ url }) => {
 	return {
 		url: url.pathname,
